fdac9dbcf5f148b7c883bd71950e78e3d0340aa2
Seerat Dheer <sdheer@purdue.edu>
2019-09-02 20:39:36 -0400

    Test

---
+++ Desktop/AmazonWarehouse/P5TestScripts/LinkedList-interview prep/src/Node.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Node {
+    private Node next;
+    private int data;
+
+    public Node (int data, Node next){
+        this.next = next;
+        this.data = data;
+    }
+
+    public Node (int data) {
+        this.data = data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public void setNext(Node next){
+        this.next = next;
+    }
+
+    public Node getNext(){
+        return this.next;
+    }
+}
